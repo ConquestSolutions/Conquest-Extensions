b7223a55094712122d78acc303bf3019494cc99e
Wassim <eywassim@gmail.com>
2015-01-12 16:28:40 +1030

    Tidy up ActionSavingExample

--- More Examples/ActionSavingValidation.py
+++ More Examples/ActionSavingValidation.py
@@ -3,15 +3,16 @@ def fail(s): callBack(s)
 def stripMargin(s):
 	import re
 	return re.sub("\s*\| ", "\n", s).strip()
-def prepareSql(sql):
-	return stripMargin(sql).replace("\n", " ")
+def runQuery(sql, f):
+	from Conquest.Silverlight import App
+	formattedSql = stripMargin(sql).replace("\n", " ")
+	App.RunQuery(formattedSql, f)
 def debug(*obs):
 	from System.Windows.MessageBox import Show
 	Show("\n".join([ str(ob) for ob in obs]))
 
 ## How to run a query before saving.
 
-from Conquest.Silverlight import App
 def databaseCheckBeforeSave():
 	"""
 	Make sure the action is not re-assigned to another standard action, once assigned.
@@ -22,12 +23,13 @@ def databaseCheckBeforeSave():
 		if collection == None or len(collection) != 1:
 			return fail("You cannot change the Standard Action once assigned.")
 		done()
-	checkSql = prepareSql("""
-	| select ActionID from tblAction where (StdActionID is null or StdActionID <= 0 or StdActionID > 0 and StdActionID = %s)
-	| and ActionID = %s;
-	""" % (str(source.StdActionID or 0), str(source.ActionID)))
-	debug(checkSql)
-	App.RunQuery(checkSql, onResult)
+	checkSql = """
+	| select ActionID from tblAction where (StdActionID is null or StdActionID <= 0 or StdActionID > 0 and StdActionID = {sid})
+	| and ActionID = {aid};
+	""".format(
+		sid=str(source.StdActionID or 0), 
+		aid=str(source.ActionID))
+	runQuery(checkSql, onResult)
 	
 ## The validation uses a callback, so we must set this flag to True
 
