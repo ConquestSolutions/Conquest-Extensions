3b5e3a07db2a6643b901084ad74e446a6681e3a0
Andy Li <andy2008@gmail.com>
2018-11-02 09:56:58 +1100

    simple lookups

--- 2018 Field Validations/03-Lookups.py
+++ 2018 Field Validations/03-Lookups.py
@@ -1,3 +1,30 @@
 
+####  Simple Lookup
+from Conquest.Silverlight.Controls import *
 
+def grid_RowDoubleClick(_, e) :
+	global popWindow
+	popWindow.Close()
+	source.Account = e.Data
+
+def showGrid(items) :
+	global popWindow
+	
+	dg = CQDataGrid()
+	dg.MaxHeight = 300
+	dg.IsReadOnly = True
+	dg.GridRowDoubleClick += grid_RowDoubleClick
+	dg.ItemsSource = items
+
+	popWindow = CQChildWindow()
+	popWindow.HasCloseButton = True
+	popWindow.Title = "Double-click a item to select"
+	popWindow.Content = dg
+	popWindow.Show()
+
+if LookupRequested or IsTestMode :
+	showGrid(['Item 1', 'Item 2', 'Item 3'])
+
+
+#### Lookup with service call
 #https://jsonplaceholder.typicode.com/users
